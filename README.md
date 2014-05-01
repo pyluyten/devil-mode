306a32b8208c4ca639afe17aaa2d7788500fbbab
Pierre-Yves Luyten <py@luyten.fr>
2014-05-01 13:42:19 +0200

    Initial push

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ devil-mode
 ==========
 
 Easily operate on text while in selection state
+Devil mode does not replace *any* key while in "normal" state.
+As soon as you toggle mark (eg, with ^SPC or ^RET), devil triggers
+some keys
+
+press $ to extend selection to end of line.
+Just look at the code and customize it.
+Its inspired from vim so w b $ ^ works.
+Or, i/j/k/l because its more efficient than hjkl
+And i kept h for help.
+
+Use tab to toggle point and mark. Use space to cancel selection.
+Note that cancelling selection might allow you to fool this mode
+to simply move on.
+
+
+
+
+
---
+++ devil-mode.el
@@ -0,0 +1,65 @@
+;;          KEYMAP
+;; --------------------------------------------
+(defvar devil-mode-map nil "Keymap for devil-mode")
+
+
+(defun devil-add-selection-keymap ()
+  (when (not devil-mode-map)
+  (setq devil-mode-map (make-sparse-keymap))
+
+  ;------------------ moves -------------------------
+  (define-key devil-mode-map (kbd "l") 'forward-char)
+  (define-key devil-mode-map (kbd "j") 'backward-char)
+  (define-key devil-mode-map (kbd "k") 'next-line)
+  (define-key devil-mode-map (kbd "i") 'previous-line)
+  (define-key devil-mode-map (kbd "$") 'end-of-line)
+  (define-key devil-mode-map (kbd "a") 'beginning-of-line)
+  (define-key devil-mode-map (kbd "^") 'back-to-indentation)
+  (define-key devil-mode-map (kbd "w") 'forward-word)
+  (define-key devil-mode-map (kbd "b") 'backward-word)
+
+  ;------------------------ more moves! ----------------
+  (define-key devil-mode-map (kbd "s") 'isearch-forward)
+
+
+  ;------------------ operators -------------------------
+  (define-key devil-mode-map (kbd "c") 'kill-ring-save)  
+  (define-key devil-mode-map (kbd "x") 'kill-region)
+  (define-key devil-mode-map (kbd "d") 'kill-region)
+
+
+  ;------------------- others -----------------------------
+  (define-key devil-mode-map (kbd "TAB") 'exchange-point-and-mark)
+  (define-key devil-mode-map (kbd "h k") 'describe-key)
+  (define-key devil-mode-map (kbd "h f") 'describe-function)
+  (define-key devil-mode-map (kbd "SPC") 'keyboard-quit)
+  )
+
+  (set-temporary-overlay-map devil-mode-map t)
+)
+
+
+;;             toggle keymap
+;; ------------------------------
+(defun devil-activate-selection-state ()
+   (devil-add-selection-keymap)
+)
+
+(defun devil-deactivate-selection-state ()
+)
+
+(add-hook 'activate-mark-hook 'devil-activate-selection-state)
+
+(add-hook 'activate-mark-hook 'devil-deactivate-selection-state)
+
+
+;;         define mode
+;; ------------------------------
+
+(define-minor-mode devil-mode
+  "Easily navigate while text is selected"
+ :lighter " dev"
+)
+
+
+(provide 'devil-mode)

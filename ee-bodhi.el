793cbab0658888078311bac1b964e57bd874ae03
Pierre-Yves Luyten <py@luyten.fr>
2014-05-23 23:59:50 +0200

    ee-bodhi: slight cleanup

--- ee-bodhi.el
+++ ee-bodhi.el
@@ -1,42 +1,13 @@
-; Principle
-; - ErgoEmacs Component for ^x motion, ^c motion
-; - ErgoEmacs Theme for paddle etc...
-
 (require 'ergoemacs-mode)
 
 (add-to-list 'load-path "./")
 (require 'bodhi-common)
 
 
-; ----------- COMMANDS ---------------------
-
- (defun bodhi-copy-line (arg)
-    "Copy lines (as many as prefix argument) in the kill ring.
-      Ease of use features:
-      - Move to start of next line.
-      - Appends the copy on sequential calls.
-      - Use newline as last char even on the last line of the buffer.
-      - If region is active, copy its lines."
-    (interactive "p")
-    (let ((beg (line-beginning-position))
-          (end (line-end-position arg)))
-      (when mark-active
-        (if (> (point) (mark))
-            (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
-          (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
-      (if (eq last-command 'copy-line)
-          (kill-append (buffer-substring beg end) (< end beg))
-        (kill-ring-save beg end)))
-    (kill-append "\n" nil)
-    (beginning-of-line (or (and arg (1+ arg)) 2))
-    (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
-
-
-
 ; ------------- COMPONENTS ---------------------------
 
 (ergoemacs-theme-component cua-prompters ()
-  "Use cua-inspired keys: s[ave], z[undo], [f]ind, [r]eplace"
+  "Use cua-inspired prompters to deploy emacs"
   (global-set-key (kbd "C-s") 'save-buffer)
   (global-set-key (kbd "C-z") 'undo)
 
@@ -49,22 +20,16 @@
 (ergoemacs-theme-component operator-motion ()
   "Use operator motion commands."
   (global-set-key (kbd "C-c c") 'body-copy-line)
-  (global-set-key (kbd "C-x x") 'kill-whole-line)
-  (global-set-key (kbd "C-x o") 'kill-word)
-  (global-set-key (kbd "C-x $") 'kill-line)
-  (global-set-key (kbd "C-x w") 'kill-word))
-
+  (global-set-key (kbd "C-x x") 'kill-whole-line))
 
 
 (ergoemacs-theme-component perl-motions ()
-  "Part of vim-like perl motions."
+  "Use $ and 0 to move to eol bol."
   (global-set-key (kbd "$")   'end-of-line)
-  (global-set-key (kbd "C-$") 'end-of-line)
-  (global-set-key (kbd "M-$") 'kill-line)
+  (global-set-key (kbd "C-$") 'kill-line)
 
-  (global-set-key (kbd "C-à") 'beginning-of-line)
-  (global-set-key (kbd "C-^") 'beginning-of-line)
-  (global-set-key (kbd "M-à") 'bodhi-backward-kill-line))
+  (global-set-key (kbd "M-0") 'ergoemacs-beginning-of-line-or-what)
+  (global-set-key (kbd "C-à") 'bodhi-backward-kill-line))
 
 ; -------------- THEME ----------------------------
 

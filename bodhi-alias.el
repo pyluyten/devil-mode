144b6d5f5de6286d21e4ec3cbfb7d4c1967e70ce
Pierre-Yves Luyten <py@luyten.fr>
2014-05-25 23:03:01 +0200

    Aliases: start with a defvar and a func to read it

--- bodhi-alias.el
+++ bodhi-alias.el
@@ -2,20 +2,53 @@
 
 ; core
 
-(defun bodhi-alias-from-strings (alname funame &optional docstr)
-  "Make an alias from strings."
+(defvar bodhi-aliases nil
+ "List of bodhi aliases.
+
+Each bodhi alias is a string of three elements,
+the alias symbol, the function it's an alias for,
+and the optional docstr.
+
+Call xxx to display existing aliases
+Call xxx to edit existing aliases and maybe save it to a file.
+Call xxx to reload aliases from the file.")
+
+
+(defun bodhi-alias-list-aliases ()
+"List current bodhi aliases.
+
+parse the full list of bodhi 'aliases' (which are themselves, list...)
+this func is really ^n slow! does not matter, unless you want
+ten thousands aliases, but you don't, dude."
   (interactive)
-  (defalias
-    (intern alname)
-    (symbol-function (intern-soft funame))
-    docstr))
+  (setq i 0)
+  (setq cural (nth i bodhi-aliases))
+  (while cural
+    (message (format "Alias Nth. %s" i))
+    (message (concat "ALIAS:" (symbol-name (nth 0 cural))
+	             "\nFUNC:"  (symbol-name (nth 1 cural))
+		     "\nDESC:"  (nth 2 cural)))
+    (setq i (+ i 1))
+    (setq cural (nth i bodhi-aliases))))
 
 
-(defun bodhi-read-lines (filePath)
-  "Return a list of lines of a file at filePath."
+
+(defun bodhi-alias-from-strings (alname funame &optional docstr)
+  "Make an alias from strings.
+
+Do not call this directly, this is made to be called by others."
+  (interactive)
+  (setq el (list (intern alname) (intern-soft funame) docstr))
+  (setq newl (cons el bodhi-aliases))
+  (setq bodhi-aliases newl)
+  (defalias (nth 0 el) (nth 1 el) (nth 2 el)))
+
+
+(defun bodhi-read-lines (fullname)
+  "Return a list of lines of a file <fullname>."
   (interactive)
   (with-temp-buffer
-    (insert-file-contents filePath)
+    (insert-file-contents fullname)
     (split-string (buffer-string) "\n" t)))
 
 
@@ -24,9 +57,9 @@
 For each row, try to create an alias from this row."
   (interactive)
   (setq parser
-    (read-lines "bodhi-alias.alias"))
+    (bodhi-read-lines "bodhi-aliases.alias"))
   (while parser
-    (setq row (split-string (car parser)))
+    (setq row (split-string (car parser) " "))
     (bodhi-alias-from-strings (nth 0 row) (nth 1 row))
     (setq parser (cdr parser))))
 
@@ -35,4 +68,6 @@ For each row, try to create an alias from this row."
 ; this major mode should
 ; 1. allow to edit a .alias with convenience, mostly completion. Maybe org.
 ; 2. distribute a default .alias
-; 3. use a variable to lookup for the file?
+; 3. use a defcustom to lookup for the file?
+; 4. have a func do display current aliases
+; 5. allow to edit aliases @ runtime or reload

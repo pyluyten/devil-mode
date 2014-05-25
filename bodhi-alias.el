298f1412997abbf5d4dbabcacb419439d2f515ce
Pierre-Yves Luyten <py@luyten.fr>
2014-05-25 23:22:48 +0200

    Alias: use | separator

--- bodhi-alias.el
+++ bodhi-alias.el
@@ -1,4 +1,19 @@
 ; this is a bare trial.
+; ok now this works, but we miss
+;
+; 1. allow to edit a .alias with convenience, mostly completion.
+;    now we have a .org file  but main features are to be coded.
+; 2. distribute a default .alias
+;    or, maybe make bodhi-common depend on this
+;    & let it ship its aliases =)
+; 3. use a defcustom to lookup for the file?
+;    or not...
+; 4. have a func do display current aliases
+;    current one is just for debugging
+;    this might be the same than 1...
+
+
+
 
 ; core
 
@@ -57,17 +72,13 @@ Do not call this directly, this is made to be called by others."
 For each row, try to create an alias from this row."
   (interactive)
   (setq parser
-    (bodhi-read-lines "bodhi-aliases.alias"))
+    (bodhi-read-lines "bodhi-alias.org"))
   (while parser
-    (setq row (split-string (car parser) " "))
-    (bodhi-alias-from-strings (nth 0 row) (nth 1 row))
+    (setq row (split-string (car parser) "|"))
+    (bodhi-alias-from-strings
+      (replace-regexp-in-string " " "" (nth 1 row))
+      (replace-regexp-in-string " " "" (nth 2 row))
+      (nth 3 row))
     (setq parser (cdr parser))))
 
 
-; major mode for .alias
-; this major mode should
-; 1. allow to edit a .alias with convenience, mostly completion. Maybe org.
-; 2. distribute a default .alias
-; 3. use a defcustom to lookup for the file?
-; 4. have a func do display current aliases
-; 5. allow to edit aliases @ runtime or reload
